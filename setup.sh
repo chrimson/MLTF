5535edfd9b584782e6a3bca0689ab55fb32b20ca
Chris Limson <chrimson@gmail.com>
2023-11-28 03:59:30 +0000

    Suppress interaction

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 export DEBIAN_FRONTEND=noninteractive
+sudo sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
+sudo sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/g" /etc/needrestart/needrestart.conf
 
 sudo apt update -y
 sudo apt upgrade -y
@@ -8,5 +10,5 @@ sudo apt install -y unzip zip
 sudo apt install -y jupyter
 
 jupyter notebook --generate-config
-sed -i "s/App\.allow_origin = ''/App.allow_origin = '*'/g" ~/.jupyter/jupyter_notebook_config.py
-sed -i "s/App\.ip = 'localhost'/App.ip = '0.0.0.0'/g" ~/.jupyter/jupyter_notebook_config.py
+sed -i "s/# c.NotebookApp\.allow_origin = ''/c.NotebookApp.allow_origin = '*'/g" ~/.jupyter/jupyter_notebook_config.py
+sed -i "s/# c.NotebookApp\.ip = 'localhost'/c.NotebookApp.ip = '0.0.0.0'/g" ~/.jupyter/jupyter_notebook_config.py
